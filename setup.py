1e5267e6798abbec8f80f96e22c76abc91566a8d
Christoph Stelz <mail@ch-st.de>
2018-03-24 17:32:42 +0100

    setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+        name = 'holmoslib',
+        packages = ['holmoslib'],
+        version = '0.1',
+        description = 'Holographic microscope image analysis software',
+        author = 'Christoph Stelz',
+        author_email = 'mail@ch-st.de',
+        url = 'https://github.com/stelzch/algo-gpu',
+        keywords = ['holography', 'science', 'imaging'],
+        classifiers = [
+            'Topic :: Scientific/Engineering :: Physics',
+            'Topic :: Scientific/Engineering :: Visualization',
+            'Programming Language :: Python :: 3 :: Only',
+            'Operating System :: POSIX :: Linux',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'Development Status :: 2 - Pre-Alpha'
+        ]
+)
